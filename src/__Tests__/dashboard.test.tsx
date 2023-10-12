ffa181f0797b3dfad772f7b28c8b14ba0288b9e4
Niranjan Lagishetti <niranjanla@cybage.com>
2023-10-12 16:51:47 +0530

    minor change

--- src/__Tests__/dashboard.test.tsx
+++ src/__Tests__/dashboard.test.tsx
@@ -1,26 +1,27 @@
 import { Dashboard } from "../components/Dashboard";
 import { fireEvent, render, screen } from '@testing-library/react';
+import { ContextProvider } from "../store/contextAPI";
 
 test('Dashbaord component rendered with Default Text', () => {
-  render(<Dashboard />);
+  render(<ContextProvider><Dashboard /></ContextProvider>);
   const TitleElement = screen.getByText(/Dashboard/i);
   expect(TitleElement).toBeInTheDocument();
 });
 
 test('Dashbaord component rendered with Button', () => {
-  render(<Dashboard />);
+  render(<ContextProvider><Dashboard /></ContextProvider>);
   const button = screen.getByTestId("add");
   expect(button).toBeInTheDocument();
 });
 
 test('Dashbaord component rendered with Cards', () => {
-  render(<Dashboard />);
+  render(<ContextProvider><Dashboard /></ContextProvider>);
   const cards = screen.getAllByTestId("cards");
   expect(cards).toHaveLength(4);
 });
 
 test('Dashbaord component with Add button action', () => {
-  render(<Dashboard />);
+  render(<ContextProvider><Dashboard /></ContextProvider>);
   const addButton = screen.getByTestId("add");
   fireEvent.click(addButton);
   const cards = screen.getAllByTestId("cards");
